52f057da618a1675938583c774690e3f67279f94
lightkun10 <lifeisubw@gmail.com>
2021-01-05 06:13:50 +0700

    Finish Blog list: 4.4: helper functions and unit tests, step2

--- bloglist-backend/utils/list_helper.js
+++ bloglist-backend/utils/list_helper.js
@@ -1,5 +1,22 @@
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) => {
+  if (blogs.length > 1) {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  }
+
+  if (blogs.length === 1) {
+    return blogs[0].likes;
+  }
+
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  return undefined;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
